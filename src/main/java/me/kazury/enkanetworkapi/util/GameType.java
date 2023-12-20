5cc2ea7046fb3de0ba5bded0eae83c0b3c74bfb2
kazuryy <121773704+kazuryyx@users.noreply.github.com>
2023-12-20 14:35:23 +0100

    dim decided to break master branch

--- src/main/java/me/kazury/enkanetworkapi/util/GameType.java
+++ src/main/java/me/kazury/enkanetworkapi/util/GameType.java
@@ -6,7 +6,7 @@ import org.jetbrains.annotations.NotNull;
  * A game type, which is used to retrieve json data by url.
  */
 public enum GameType {
-    GENSHIN("https://gitlab.com/Dimbreath/AnimeGameData/-/raw/master/%s/%s"),
+    GENSHIN("https://gitlab.com/Dimbreath/AnimeGameData/-/raw/main/%s/%s"),
     HONKAI("https://raw.githubusercontent.com/Dimbreath/StarRailData/59d64be43a1da285cf22ba9be5ed90ef2b23f857/%s/%s");
 
     @NotNull
